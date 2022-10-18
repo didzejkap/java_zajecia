0977a0c6f0018208e1644392bd2b5a4249816a1a
KacperN <switchgaming74@gmail.com>
2022-10-18 14:23:47 +0200

    Add files via upload

---
+++ jsf_03/src/jsfactivator/ActivatorJSF23.java
@@ -0,0 +1,11 @@
+package jsfactivator;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+import jakarta.faces.annotation.FacesConfig.Version;
+
+@FacesConfig(version = Version.JSF_2_3)
+@ApplicationScoped
+public class ActivatorJSF23 {
+
+}
